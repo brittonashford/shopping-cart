c0380882f4fb269a130d6f6909b85fe2c399df52
Bret Kitchell <brittonashford@gmail.com>
2022-07-14 21:55:43 -0500

    Home, Cart and Single Product built out. Need to finish filters

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import { CartState } from "../context/Context";
+import SingleProduct from "./SingleProduct";
+import Filters from "./Filters";
+import './styles.css';
+
+
+
+const Home = () => {
+
+    const {
+        state: { products },
+    } = CartState();
+
+    return (
+        <div className="home">
+        <Filters />
+            <div className="productContainer">
+                {
+                    products.map((prod) => {
+                        return <SingleProduct prod={prod} id={prod.id} />;
+                    })
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Home;
